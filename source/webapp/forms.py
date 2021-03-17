b721627276a61beab52967137b6e43a0cc68b9ef
karlygach <karygachkenenbaeva@gmail.com>
2021-03-17 17:18:32 +0600

    изменена форма

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from django.forms import widgets
 
-from webapp.models import Type, Status
+from webapp.models import Type, Status, Issue
 
 
-class IssueForm(forms.Form):
-    summary = forms.CharField(max_length=130, required=True, label='Название')
-    description = forms.CharField(max_length=3000, required=False, widget=widgets.Textarea, label='Подробнее о задаче')
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Тип',
-                                          widget=forms.CheckboxSelectMultiple)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус')
+class IssueForm(forms.ModelForm):
 
+    class Meta:
+        model = Issue
+        fields = ['summary', 'description', 'type', 'status']
