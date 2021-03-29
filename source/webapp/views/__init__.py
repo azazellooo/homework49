a1bd8fd0f89246bf28f9de8c645ea329582531fe
karlygach <karygachkenenbaeva@gmail.com>
2021-03-29 18:45:43 +0600

    добавлены импорты представлений

---
+++ source/webapp/views/__init__.py
@@ -0,0 +1,9 @@
+from webapp.views.issue_views import (
+    IssueListView,
+    IssueView,
+    IssueCreateView,
+    IssueUpdate,
+    IssueDelete,
+    )
+
+from webapp.views.project_views import ProjectListView, ProjectDetailView, ProjectCreateView
