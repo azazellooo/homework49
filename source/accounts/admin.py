185b8beb9ed27c63f7d61fac452cc79e69b7245a
karlygach <karygachkenenbaeva@gmail.com>
2021-04-16 17:25:07 +0600

    добавлены пользователям поля для аватарки, для ссылки github и поле о себе

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import Profile
+from django.contrib.auth import get_user_model
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'github', 'about_user']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
 # Register your models here.
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    github = models.URLField(null=True, blank=True, max_length=250, verbose_name='Ссылка на github')
+    about_user = models.TextField(null=True, blank=True, verbose_name='О себе', max_length=3000)
 # Create your models here.
