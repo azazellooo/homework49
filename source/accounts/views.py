5cb43ed13973ce8d5916a65fae160a3e7d3bf05c
karlygach <karygachkenenbaeva@gmail.com>
2021-04-19 15:58:04 +0600

    создана страница детального просмотра личной страницы пользователя

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 from accounts.forms import UserRegisterForm
+
+from accounts.models import Profile
 
 
 def register_view(request, **kwargs):
@@ -11,8 +14,20 @@ def register_view(request, **kwargs):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            Profile.objects.create(user=user)
             return redirect('project-list')
     context['form'] = form
     return render(request, 'registration/register.html', context=context)
+
+
+class UserDetailView(DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_object'
+
+    def get_context_data(self, **kwargs):
+        projects = self.object.project.all()
+        kwargs['projects'] = projects
+        return super().get_context_data(**kwargs)
 
 # Create your views here.
