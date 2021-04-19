7dc0414c3371efa6a6d2bc8340db3389b01d00e3
karlygach <karygachkenenbaeva@gmail.com>
2021-04-19 15:53:36 +0600

    создана модель Профиля пользователя

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name='Пользователь')
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
     avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
     github = models.URLField(null=True, blank=True, max_length=250, verbose_name='Ссылка на github')
     about_user = models.TextField(null=True, blank=True, verbose_name='О себе', max_length=3000)
