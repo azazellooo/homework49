260c19294c3d5d06cafa1dc875901bd094427c65
karlygach <karygachkenenbaeva@gmail.com>
2021-04-22 17:33:05 +0600

    исправлен импорт

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import PasswordInput
-from xdg.Exceptions import ValidationError
 
 from accounts.models import Profile
 
